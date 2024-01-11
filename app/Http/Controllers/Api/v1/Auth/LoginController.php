69d2e836e0f65170f35cc0e9e078ed032429008e
Genesis John Abobo <geiizanityy@gmail.com>
2024-01-11 14:40:22 +0800

    Auth setup

--- app/Http/Controllers/Api/v1/Auth/LoginController.php
+++ app/Http/Controllers/Api/v1/Auth/LoginController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\LoginRequest;
+
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
+
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request): JsonResponse
     {
-        return response()->json(['data' => 'Login.']);
+
+        $credentials = $request->validated();
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            $hashToken = $user->createToken('access_token')->plainTextToken;
+            return $this->sendSuccessResponse(['user' => $user,'token'=> $hashToken],'User successfully logged in.');
+        }
+        return $this->sendFailedResponse('Credentials not found',401);
+
+
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendSuccessResponse($data, $message): JsonResponse {
+        return response()->json(['success' => true, 'data' => $data,'message'=> $message],200);
+    }
+    public function sendFailedResponse($message,$code): JsonResponse {
+        return response()->json(['success' => false,'error'=> $message],$code);
+    }
 }
