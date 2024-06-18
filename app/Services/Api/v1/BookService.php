56899eac377c20870d0e2502490b8cd352a964d3
Genesis John Abobo <geiizanityy@gmail.com>
2024-06-18 11:22:35 +0800

    Create book with multiple accounts

--- app/Services/Api/v1/BookService.php
+++ app/Services/Api/v1/BookService.php
@@ -45,7 +45,7 @@ class BookService
 
         DB::transaction(function () use ($attribute) {
             $book = Book::create($attribute);
-            $book->accounts()->attach($attribute['account_id']);
+            $book->accounts()->attach($attribute['account_ids']);
             $book->account_groups()->attach([
                 $attribute['account_group_id']
             ]);
