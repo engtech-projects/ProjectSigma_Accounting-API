269a7855c89a9939baf43d9732014b8bfc747f60
ryanpastoriza <ryanpastoriza@gmail.com>
2024-09-02 13:27:08 +0800

    add book column account_group_id

--- database/migrations/2024_09_02_052050_add_column_book_table.php
+++ database/migrations/2024_09_02_052050_add_column_book_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::table('books', function (Blueprint $table) {
             $table->unsignedBigInteger('account_group_id')->nullable()->after('symbol');
         });
     }
