ae1a16738399a4683ac2847d25e10cc6b6a9392a
Rustom Pedales <pedales.rustom@gmail.com>
2024-11-22 07:10:12 +0800

    [Update] : remove unique prf_no

---
+++ database/migrations/2024_11_21_230533_remove_prf_unique.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->dropUnique('payment_request_prf_no_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->unique('prf_no', 'payment_request_prf_no_unique');
+        });
+    }
+};
+
