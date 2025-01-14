c5ddb4232736dbacd7bed2bb2ef929cd686a3971
Rustom Pedales <pedales.rustom@gmail.com>
2025-01-14 14:24:31 +0800

    Revert "[BugFix]: undefined approvals expect and no error handler

--- app/Http/Resources/AccountingCollections/PaymentRequestCollection.php
+++ app/Http/Resources/AccountingCollections/PaymentRequestCollection.php
@@ -37,9 +37,9 @@ class PaymentRequestCollection extends JsonResource
             'next_approval' => $this->getNextPendingApproval(),
             'details' => $details,
             'step_approval' => [
-                'payment_request' => new ApprovalAttributeResource(['approvals' => $this->approvals]),
-                'disbursement_voucher' => new ApprovalAttributeResource(['approvals' => $this->journalEntry->first()->voucher()->first()->approvals]),
-                'cash_voucher' => new ApprovalAttributeResource(['approvals' => $this->journalEntry->last()->voucher()->first()->approvals]),
+                'payment_request' => new ApprovalAttributeResource(['approvals' => $this->approvals ?? []]),
+                'disbursement_voucher' => new ApprovalAttributeResource(['approvals' => $this->journalEntry->first()->voucher()->first()->approvals ?? []]),
+                'cash_voucher' => new ApprovalAttributeResource(['approvals' => $this->journalEntry->last()->voucher()->first()->approvals ?? []]),
             ]
         ]);
     }
