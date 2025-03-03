88e518b3b528fdbb705d7bbf0bf838c9f6494b73
Ephraim Talaba <eptals2@gmail.com>
2025-03-03 13:44:55 +0800

    Update TermsService.php

--- app/Services/TermsService.php
+++ app/Services/TermsService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Http\Resources\TermsCollection;
-use App\Http\Resources\TermsCollections;
 use App\Models\Term;
 
 class TermsService
@@ -11,10 +10,10 @@ class TermsService
     public static function getPaginated(array $validateData)
     {
         $queryTermsRequest = Term::when(isset($validateData['key']), function ($query, $key) use ($validateData) {
-            $query->where('name', 'like', '%' . $validateData['key'] . '%');
+            $query->where('name', 'like', '%'.$validateData['key'].'%');
         })
             ->paginate(config('services.pagination.limit'));
 
-        return (TermsCollection::collection($queryTermsRequest)->response()->getData(true));
+        return TermsCollection::collection($queryTermsRequest)->response()->getData(true);
     }
 }
