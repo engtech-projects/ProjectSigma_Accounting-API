c674e082ef811ea21285232716eb12f60a81c164
eptals1 <eptals2@gmail.com>
2025-04-11 19:40:01 +0800

    Create 2025_04_10_140817_change_opening_balances_columns_order.php

---
+++ database/migrations/2025_04_10_140817_change_opening_balances_columns_order.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //Change columns order in opening_balances table
+        Schema::table('opening_balances', function (Blueprint $table) {
+            $table->string('created_at')->after('posting_period_id')->change();
+            $table->string('updated_at')->nullable()->after('created_at')->change();
+            $table->string('deleted_at')->nullable()->after('updated_at')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //Restore default columns order in opening_balances table
+        Schema::table('opening_balances', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('opening_balance', 10, 2)->after('id')->change();
+            $table->decimal('remaining_balance', 10, 2)->after('opening_balance')->change();
+            $table->bigInteger('account_id')->after('remaining_balance')->change();
+            $table->bigInteger('posting_period_id')->after('account_id')->change();
+            $table->string('created_at')->after('posting_period_id')->change();
+            $table->string('updated_at')->nullable()->after('created_at')->change();
+            $table->string('deleted_at')->nullable()->after('updated_at')->change();
+        });
+    }
+};
