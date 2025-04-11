007b0df9980fa548bfc349f43be10498feb69062
eptals2 <eptals2@gmail.com>
2025-04-11 20:20:17 +0800

    Update 2025_04_10_132738_change_account_types_columns_order.php

--- database/migrations/2025_04_10_132738_change_account_types_columns_order.php
+++ database/migrations/2025_04_10_132738_change_account_types_columns_order.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,9 +13,9 @@ return new class extends Migration {
     {
         //Change columns order in account_types table
         Schema::table('account_types', function (Blueprint $table) {
-            $table->string('created_at')->after('notation')->change();
-            $table->string('updated_at')->nullable()->after('created_at')->change();
-            $table->string('deleted_at')->nullable()->after('updated_at')->change();
+            $table->timestamp('created_at')->nullable()->after('notation')->change();
+            $table->timestamp('updated_at')->nullable()->after('created_at')->change();
+            $table->timestamp('deleted_at')->nullable()->after('updated_at')->change();
         });
     }
 
@@ -30,9 +31,9 @@ return new class extends Migration {
             $table->enum('account_category', ['asset', 'equity', 'expenses', 'income', 'liabilities', 'revenue', 'capital'])->after('account_type')->change();
             $table->enum('balance_type', ['debit', 'credit'])->after('account_category')->change();
             $table->enum('notation', ['+', '-'])->after('balance_type')->change();
-            $table->string('created_at')->after('notation')->change();
-            $table->string('updated_at')->nullable()->after('created_at')->change();
-            $table->string('deleted_at')->nullable()->after('updated_at')->change();
+            $table->timestamp('created_at')->nullable()->after('notation')->change();
+            $table->timestamp('updated_at')->nullable()->after('created_at')->change();
+            $table->timestamp('deleted_at')->nullable()->after('updated_at')->change();
         });
     }
 };
