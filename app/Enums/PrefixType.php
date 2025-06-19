6afc73df98159dce2921332aa342141737d796b4
Rustom Pedales <pedales.rustom@gmail.com>
2025-06-19 15:01:32 +0800

    [Update]: prefix payment request code dynamic based on department

--- app/Enums/PrefixType.php
+++ app/Enums/PrefixType.php
@@ -7,9 +7,6 @@ use App\Enums\Traits\EnumHelper;
 enum PrefixType: string
 {
     use EnumHelper;
-
-    case RFA_ACS = 'RFA-ACS';
-    case RFA_PAYROLL = 'RFA-PAYROLL';
     case PRF_ACS = 'PRF-ACS';
     case PRF_PAYROLL = 'PRF-PAYROLL';
 
