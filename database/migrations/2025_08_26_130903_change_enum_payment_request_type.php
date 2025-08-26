3344e7d6506d1b616851834e27d2236903ae1fc0
Rustom Pedales <pedales.rustom@gmail.com>
2025-08-26 13:16:35 +0800

    [CodeRabbit-fix]: Missing PO handling — critical additions required
    change type from string to enum

---
+++ database/migrations/2025_08_26_130903_change_enum_payment_request_type.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\PaymentRequestType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->enum('type', [
+                PaymentRequestType::PRF->value,
+                PaymentRequestType::PAYROLL->value,
+                PaymentRequestType::PO->value
+            ])->default(PaymentRequestType::PRF->value);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+
+        Schema::table('payment_request', function (Blueprint $table) {
+            $table->enum('type', PaymentRequestType::toArray())
+                ->default(PaymentRequestType::PRF->value);
+        });
+    }
+};
