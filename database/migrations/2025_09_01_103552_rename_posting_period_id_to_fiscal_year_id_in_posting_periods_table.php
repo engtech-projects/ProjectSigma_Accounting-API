0c5fdc73805ce2454eaac5bd467fcd7c4b2e9c35
Cesar Ian Apatino <gameboy321dats@gmail.com>
2025-09-03 01:13:11 -0700

    Use native schema operations in migration scripts

--- database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
+++ database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Schema;
 return new class () extends Migration {
     public function up(): void
     {
+        Schema::useNativeSchemaOperationsIfPossible();
         Schema::table('posting_periods', function (Blueprint $table) {
             $table->renameColumn('posting_period_id', 'fiscal_year_id');
         });
+        Schema::useNativeSchemaOperationsIfPossible(false);
     }
 
     public function down(): void
