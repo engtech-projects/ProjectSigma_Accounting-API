251fa781247fd3f7bc210d52fc6ace646d4ce64f
Cesar Ian Apatino <gameboy321dats@gmail.com>
2025-09-04 08:42:01 -0700

    Revert "Simplify migration column renaming logic"
    This reverts commit 78940ee43281347b200951916cfa415a08c0e94d.

--- database/migrations/2025_09_01_020819_rename_period_id_to_fiscal_year_id_in_journal_entry.php
+++ database/migrations/2025_09_01_020819_rename_period_id_to_fiscal_year_id_in_journal_entry.php
@@ -1,18 +1,35 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::statement('ALTER TABLE `journal_entry` CHANGE `posting_period_id` `fiscal_year_id` BIGINT UNSIGNED NOT NULL');
-        DB::statement('ALTER TABLE `journal_entry` CHANGE `period_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
+        Schema::useNativeSchemaOperationsIfPossible();
+        if (Schema::hasTable('journal_entry')) {
+            if (Schema::hasColumn('journal_entry', 'posting_period_id') && !Schema::hasColumn('journal_entry', 'fiscal_year_id')) {
+                DB::statement('ALTER TABLE `journal_entry` CHANGE `posting_period_id` `fiscal_year_id` BIGINT UNSIGNED NOT NULL');
+            }
+            if (Schema::hasColumn('journal_entry', 'period_id') && !Schema::hasColumn('journal_entry', 'posting_period_id')) {
+                DB::statement('ALTER TABLE `journal_entry` CHANGE `period_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
+            }
+        }
+        Schema::useNativeSchemaOperationsIfPossible(false);
     }
 
     public function down(): void
     {
-        DB::statement('ALTER TABLE `journal_entry` CHANGE `posting_period_id` `period_id` BIGINT UNSIGNED NOT NULL');
-        DB::statement('ALTER TABLE `journal_entry` CHANGE `fiscal_year_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
+        Schema::useNativeSchemaOperationsIfPossible();
+        if (Schema::hasTable('journal_entry')) {
+            if (Schema::hasColumn('journal_entry', 'posting_period_id') && !Schema::hasColumn('journal_entry', 'period_id')) {
+                DB::statement('ALTER TABLE `journal_entry` CHANGE `posting_period_id` `period_id` BIGINT UNSIGNED NOT NULL');
+            }
+            if (Schema::hasColumn('journal_entry', 'fiscal_year_id') && !Schema::hasColumn('journal_entry', 'posting_period_id')) {
+                DB::statement('ALTER TABLE `journal_entry` CHANGE `fiscal_year_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
+            }
+        }
+        Schema::useNativeSchemaOperationsIfPossible(false);
     }
 };
