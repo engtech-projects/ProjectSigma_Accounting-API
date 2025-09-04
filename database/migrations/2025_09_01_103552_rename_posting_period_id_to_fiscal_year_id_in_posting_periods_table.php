9e2ac69dbf880f797b09c652c2eb5a5847b28d9f
Cesar Ian Apatino <gameboy321dats@gmail.com>
2025-09-04 08:43:24 -0700

    Revert "Reapply "Simplify migration column renaming logic""
    This reverts commit 32dc6685bf448a176a73d3fb033d5abeeabd9e2a.

--- database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
+++ database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
@@ -1,15 +1,25 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
     public function up(): void
     {
-        DB::statement('ALTER TABLE posting_periods RENAME COLUMN posting_period_id TO fiscal_year_id');
+        Schema::useNativeSchemaOperationsIfPossible();
+        Schema::table('posting_periods', function (Blueprint $table) {
+            $table->renameColumn('posting_period_id', 'fiscal_year_id');
+        });
+        Schema::useNativeSchemaOperationsIfPossible(false);
     }
+
     public function down(): void
     {
-        DB::statement('ALTER TABLE posting_periods RENAME COLUMN fiscal_year_id TO posting_period_id');
+        Schema::useNativeSchemaOperationsIfPossible();
+        Schema::table('posting_periods', function (Blueprint $table) {
+            $table->renameColumn('fiscal_year_id', 'posting_period_id');
+        });
+        Schema::useNativeSchemaOperationsIfPossible(false);
     }
 };
