9074da15187cbfc2a39c91132e0ad515e4cfab26
Cesar Ian Apatino <gameboy321dats@gmail.com>
2025-09-04 21:46:43 -0700

    Update 2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php

--- database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
+++ database/migrations/2025_09_01_103552_rename_posting_period_id_to_fiscal_year_id_in_posting_periods_table.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\DB;
 return new class () extends Migration {
     public function up(): void
     {
-        DB::statement('ALTER TABLE `posting_periods` CHANGE `posting_period_id` `fiscal_year_id` BIGINT UNSIGNED NOT NULL');
+        DB::statement('ALTER TABLE `posting_periods` CHANGE COLUMN `posting_period_id` `fiscal_year_id` BIGINT UNSIGNED NOT NULL');
     }
 
     public function down(): void
     {
-        DB::statement('ALTER TABLE `posting_periods` CHANGE `fiscal_year_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
+        DB::statement('ALTER TABLE `posting_periods` CHANGE COLUMN `fiscal_year_id` `posting_period_id` BIGINT UNSIGNED NOT NULL');
     }
 };
