71cfc8fd2f83a9b0468a7fa96d19adee4f2d981e
Rustom Pedales <pedales.rustom@gmail.com>
2025-10-14 11:47:32 +0800

    Update RequestVoucherForApprovedNotification.php

--- app/Notifications/RequestVoucherForApprovedNotification.php
+++ app/Notifications/RequestVoucherForApprovedNotification.php
@@ -42,7 +42,7 @@ class RequestVoucherForApprovedNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            'message' => 'A request for '. ucfirst($this->model->type) .' voucher has been DENIED.',
+            'message' => 'A request for '. ucfirst($this->model->type) .' voucher has been APPROVED.',
             'module' => 'Accounting',
             'request_type' =>  ucfirst($this->model->type),
             'request_id' => $this->model->id,
